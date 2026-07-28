82afadf08538c811e3bdfc130207561cf84a3d58
sanghunyun <godduk7772@gmail.com>
2026-07-28 11:03:53 +0900

    print  구문추가

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 print("안녕히세요")
 print("hello")
 print("hello2")
+print("파이썬")
--- hello2.py
+++ hello2.py
@@ -1,3 +1,11 @@
 print("hello")
 print("수정")
 print("main hello2")
+
+
+
+print("main branch는 배포되는것")
+print("main branch를 복제해서 만들고 개인이 작업한다.")
+print()
+
+
